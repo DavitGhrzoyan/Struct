7ab9e0dae691be3236d5adf904ccf04c030d63ba
Davit <ghrzoyandavit@gmail.com>
2022-12-14 21:44:58 +0400

    Add project files.

---
+++ Struct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+         struct Person
+        {
+            public string name;
+            public int age;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Person point = new Person();
+            point.age = 27;
+            point.name = "Davit";
+
+                Console.WriteLine(point.name +"  "+ point.age );
+        }
+       
+
+
+    }
+}
